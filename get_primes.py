c4d61b37a174a31c6607f8fb489061397ea60e06
akuthedeveloper <kevakuviza@gmail.com>
2016-11-30 17:17:36 +0300

    Add files via upload

--- get_primes.py
+++ get_primes.py
@@ -1,44 +1,33 @@
 def isPrime(num):
  """Checks num for primality. Returns bool."""
     if num == 2:
-return True
+        return True
      elif num < 2 or not num % 2: # even numbers > 2 not prime
-
         return False
 
      
 
             # factor can be no larger than the square root of num
 
-        for i in range(3, int(num ** .5 + 1), 2):
+    for i in range(3, int(num ** .5 + 1), 2):
 
-            if not num % i: return False
+        if not num % i: return False
 
-        return True
+    return True
 
-     
-
-     
-
-     
-
-    def generatePrimes(n):
+def generatePrimes(n):
 
         """Returns a list of prime numbers with length n"""
 
-         
+    primes = [2,]
 
-        primes = [2,]
+    noOfPrimes = 1  # cache length of primes for speed
 
-        noOfPrimes = 1  # cache length of primes for speed
+    testNum = 3 # number to test for primality
 
-        testNum = 3 # number to test for primality
+    while noOfPrimes < n:
 
-     
-
-        while noOfPrimes < n:
-
-            if isPrime(testNum):
+        if isPrime(testNum):
 
                 primes.append(testNum)
 
